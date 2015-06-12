3bc5396a98378d8f8862455e0f07edac39da5f4f
kipli <adi.gilang80@gmail.com>
2015-06-12 20:32:23 +0700

    mengedit syntak

--- syntak/1_syntak.rb
+++ syntak/1_syntak.rb
@@ -1,2 +1,23 @@
+#cout seperti kayak di c++
+puts "Hello, saya bayu mezi!";
+#
+print <<bmap
+    saya baru belajar ruby
+    ini syntak pertama saya.
+bmap
 
-puts "Hello, Ruby!";
+print <<"bmap";                # same as above
+    saya sedang belajar bersama kelompok saya
+    bantu saya jika saya kesusahan.
+bmap
+
+print <<`bmap`                 # execute commands
+	echo saya dari lampung
+	echo saya tinggal di jogja
+bmap
+
+print <<"bmap", <<"bayu"  # you can stack them
+	saya disini
+bmap
+	kamu disana
+bayu
