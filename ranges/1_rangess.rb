232b65ec9cb81dfcb7c88651442d54ed9f6b8035
kipli <adi.gilang80@gmail.com>
2015-06-15 10:45:56 +0700

    menerapkan metode yang memungkinkan Anda untuk menguji isinya

---
+++ ranges/1_rangess.rb
@@ -0,0 +1,16 @@
+# Rentang menerapkan metode yang memungkinkan Anda untuk menguji isinya dalam berbagai cara 
+digits = 0..9
+
+puts digits.include?(5)
+ret = digits.min
+puts "Min value is #{ret}"
+
+ret = digits.max
+puts "Max value is #{ret}"
+
+ret = digits.reject {|i| i < 5 }
+puts "Rejected values are #{ret}"
+
+digits.each do |digit|
+   puts "In Loop #{digit}"
+end
